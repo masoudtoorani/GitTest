decde1e7384b43d01197c385730924049dff9b77
Masoud Toorani <>
2022-01-06 22:41:33 +0300

    First Commit

---
+++ GitTestProject/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  GitTestProject
+//
+//  Created by Masoud Toorani on 6.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("git")
+        print("git2")
+    }
+
+
+}
+
